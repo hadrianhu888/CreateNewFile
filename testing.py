d830c0bda7b13fa0ffa83a88166f7c1f0c33a2c8
hadrianhu888 <hadrian.hu@gmail.com>
2023-10-07 12:26:17 -0400

    Added testing,py

---
+++ testing.py
@@ -0,0 +1,11 @@
+"""Testing.py - testing if the code works as expected."""
+
+
+def test(): 
+    """Testing the code."""
+    print("Testing the code.")
+    
+    
+if __name__ == "__main__":
+    test()
+    
