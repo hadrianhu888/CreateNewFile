fc122c8fcf321be79145157db0b20a1aec0492c8
hadrianhu888 <hadrian.hu@gmail.com>
2023-10-07 12:32:02 -0400

    minor changes to testing,py

--- testing.py
+++ testing.py
@@ -1,11 +1,10 @@
 """Testing.py - testing if the code works as expected."""
 
 
-def test(): 
+def test():
     """Testing the code."""
     print("Testing the code.")
-    
-    
+
+
 if __name__ == "__main__":
     test()
-    
